66601178b935af6ff7665300bc3286fe2d5dcbdd
yadavsakshi1302 <120712187+yadavsakshi1302@users.noreply.github.com>
2023-07-03 13:16:27 +0530

    Add files via upload

---
+++ snowpack.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    [
+      '@snowpack/plugin-typescript',
+      {
+        
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
+  ],
+  routes: [
+    
+  ],
+  optimize: {
+    
+  },
+  packageOptions: {
+   
+  },
+  devOptions: {
+    
+  },
+  buildOptions: {
+    
+  },
+};
